03c5e495f8e352ac9ddcfb13ff3a57481d586b17
EricCollado <ericcollado04@gmail.com>
2024-09-09 16:54:52 -0400

    restaurantPrices task

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $24', 'Pasta - $21', 'Burger - $15.99', 'Salmon $26'];
+const dessertMenu = ['Cake - $18.99', 'Ice Cream - $11.99', 'Pudding - $10', 'Fruit Salad - $9.99'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
